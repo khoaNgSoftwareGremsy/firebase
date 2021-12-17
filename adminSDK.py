53265affd241ef27d69daeeace60209a300356a6
khoaNgSoftware <khoa.tran@gremsy.com>
2021-12-17 09:56:06 +0700

    add sthing

--- adminSDK.py
+++ adminSDK.py
@@ -1,45 +1,70 @@
-from __future__ import print_function
-from os import stat_result
-import firebase_admin
-
-from firebase_admin import credentials
-from firebase_admin import firestore
-from numpy import isnan
-import string
 import pandas as pd
+import string
+from numpy import isnan
+from firebase_admin import firestore
+from firebase_admin import credentials
+import firebase_admin
+from os import stat_result
+import math
+# from __future__ import print_function
 
-#add credentials to app 
-cred = credentials.Certificate("/home/khoa/Gremsy/project_pLife/firebase/firebase/gsim-488be-firebase-adminsdk-kn2dg-33dc25f8ed.json")
+
+# add credentials to app
+cred = credentials.Certificate(
+    "/home/khoa/Gremsy/project_pLife/firebase/firebase/gsim_json.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-data = pd.read_excel (r'data.xlsx', sheet_name= 0)
+data = pd.read_excel(r'data.xlsx', sheet_name=0)
 
-needAtr = ['Loại hàng','Nhóm hàng(3 Cấp)','Mã hàng','Mã vạch','Tên hàng','Tồn kho','Thương hiệu','Hình ảnh (url1,url2...)']
+needAtr = ['Loại hàng', 'Nhóm hàng', 'Mã hàng', 'Mã vạch',
+           'Tên hàng', 'Tồn kho', 'Thương hiệu', 'Hình ảnh (url1,url2...)']
 
-#size of data in rectangle shape
+# size of data in rectangle shape
 sizeOfData = data.shape
 numOfEle = sizeOfData[0]
 
 dict = {}
+for i in range(0, 10):
+    print("Hello World")
 
-for k in range(0,numOfEle):
+
+def switch_type(variable):
+    if isinstance(variable, str) == False:
+        if math.isnan(variable):
+            variable = str(variable)
+            variable = " "
+            print("new var: " + variable)
+    return variable
+
+
+for k in range(0, numOfEle):
     print("current : [" + str(k) + "]")
     for i in data.columns:
         for j in needAtr:
             if i == j:
-                dict[j] = data.at[k,i]  
-                if j == 'Nhóm hàng(3 Cấp)' and dict[j].find('/') != -1: 
-                    dict[j] = dict[j].replace("/","|")
+                dict[j] = data.at[k, i]
+
+                if j == 'Nhóm hàng' and dict[j].find('/') != -1:
+                    dict[j] = dict[j].replace("/", "|")
+
+    dict['Thương hiệu'] = switch_type(dict['Thương hiệu'])
+    dict['Hình ảnh (url1,url2...)'] = switch_type(
+        dict['Hình ảnh (url1,url2...)'])
+    dict['Mã vạch'] = switch_type(dict['Mã vạch'])
+
+    print(dict['Thương hiệu'])
     attribute = {
-        u'Tên hàng': dict['Tên hàng'],
-        u'Mã hàng': dict['Mã hàng'],
-        u'Mã vạch': dict['Mã vạch'],
-        u'Tồn kho': dict['Tồn kho'],
-        u'Thương hiệu':dict['Thương hiệu'],
-        u'Hình ảnh': dict['Hình ảnh (url1,url2...)'],
+        u'prodName': dict['Tên hàng'],
+        u'prodType': dict['Loại hàng'],
+        u'prodGroup': dict['Nhóm hàng'],
+        u'prodSeri': dict['Mã hàng'],
+        u'prodBar': dict['Mã vạch'],
+        u'prodInve': dict['Tồn kho'],
+        u'prodBrand': dict['Thương hiệu'],
+        u'proImg': dict['Hình ảnh (url1,url2...)'],
     }
+    tempID = dict['Mã hàng']
 
-    db.collection(u'WareHouse').document(dict['Loại hàng']).collection(dict['Nhóm hàng(3 Cấp)']).document().set(attribute)
-
+    db.collection(u'WareHouse').document(tempID).set(attribute)
